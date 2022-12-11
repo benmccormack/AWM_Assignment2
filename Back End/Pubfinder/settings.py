7aaf9f6fad1f35398b2f97157be0099a2ea134ec
benmccormack <c19459424@mytudublin.ie>
2022-12-11 23:39:07 +0000

    small updates and comments

--- Back End/Pubfinder/settings.py
+++ Back End/Pubfinder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = #removed secret key
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -86,7 +86,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "Pubfinder.wsgi.application"
 
-
+#
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
